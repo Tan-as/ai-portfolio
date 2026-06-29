154f1325860795b4397a03e5bdb3b411a59c5935
Tan-as <untex456@gmail.com>
2026-06-29 12:14:17 +0100

    first commit - environment test

---
+++ test.py
@@ -0,0 +1,7 @@
+print("Environment working")
+name = "Antanas"
+print(f"Hello {name}, your Python is set up correctly")
+
+numbers = [1, 2, 3, 4, 5]
+total = sum(numbers)
+print(f"Sum of {numbers} = {total}")
